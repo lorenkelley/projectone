599ff78d398a1faebeda1ac90aa3166c1dc7a962
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-8.local>
2018-10-12 00:07:38 -0400

    commit styling landing page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // objective: create a function that will make the .card dispear and apply a backgroundcolor to the td element
 // step one: get the element apply event listener
-document.getElementById("tdOne").addEventListener("mouseover", hover)
+document.getElementById("tdOne").addEventListener("mouseover", hoverTD)
 // go to the html and grab the element (tdOne) and listen for the user to hover (mouseover) 
 
 // step two: create a function
-function hover() {
+function hoverTD() {
     // declare function 
     document.getElementById("tdOne").style.background = "red"
     // do this : once the html element is grabbed apply css styling 
 }
+
+// objective: create a function to apply the card values 100, 200, etc. 
