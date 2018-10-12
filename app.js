5a29d303a320dcda4ffeaac85bf8bd4cea98661f
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-8.local>
2018-10-12 16:10:28 -0400

    commit adding modal

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // objective: create a function that will make the .card dispear and apply a backgroundcolor to the td element
 // step one: get the element apply event listener
-document.getElementById("tdOne").addEventListener("mouseover", hoverTD)
+document.getElementById("tdOne").addEventListener("click", hoverTD)
 // go to the html and grab the element (tdOne) and listen for the user to hover (mouseover) 
 
 // step two: create a function
 function hoverTD() {
     // declare function 
-    document.getElementById("tdOne").style.background = "olive"
+    document.getElementById("tdOne").style.background = "#EAA1CC"
     // do this : once the html element is grabbed apply css styling 
-    document.getElementById("tdOne").innerHTML = "100"
+    document.getElementById("tdOne").innerHTML = ""
     // get the html element that needs to have the number values added to it    
 }
-
-
-
+function clickMe(){
+    document.getElementById("two").innerHTML = "HELLO"
+}
