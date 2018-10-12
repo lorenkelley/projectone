eea9bbee15a146510baf4c09bd755c50784740ff
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-8.local>
2018-10-12 16:50:35 -0400

    commit trying to  create modal

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function hoverTD() {
     // declare function 
     document.getElementById("tdOne").style.background = "#EAA1CC"
     // do this : once the html element is grabbed apply css styling 
-    document.getElementById("tdOne").innerHTML = ""
+    document.getElementById("tdOne").innerHTML = "WHAT IS THE MOVIE"
     // get the html element that needs to have the number values added to it    
 }
 function clickMe(){
