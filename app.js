b5442a94c0c8da343c7ca9f84c18a5b03620b4b7
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-8.local>
2018-10-12 23:09:08 -0400

    commit attempted to get elements by tag name in order to apply new attributes

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 // objective: create a function that will make the .card dispear and apply a backgroundcolor to the td element
 // step one: get the element apply event listener
-document.getElementById("tdOne").addEventListener("click", hoverTD)
+document.getElementsByTagName("td").addEventListener("click", hoverTD)
 // go to the html and grab the element (tdOne) and listen for the user to hover (mouseover) 
 
 // step two: create a function
 function hoverTD() {
     // declare function 
-    document.getElementById("tdOne").style.background = "#EAA1CC"
+    document.getElementsByTagName('td').style.background = "#EAA1CC"
     // do this : once the html element is grabbed apply css styling 
-    document.getElementById("tdOne").innerHTML = "<span style= 'font-size: 40px'> Who  </span>"
-    // get the html element that needs to have the number values added to it    
+    document.getElementsByTagName('td').innerHTML = "<span style= 'font-size: 40px'> Who  </span>"
+    // get the html element that needs to have the questions added to it    
     // var btn = document.createElement("button")
     // btn.innerHTML = "test"
     // document.getElementById("tdOne").appendChild(btn)
-    $('#tdOne').append(`<div>
+    $('td').append(`<div>
    <div><button onClick="clicked()">Button 2</button></div>
    </div>`)
+//    go get the elementand add a button to it 
 }
 
-function clickMe() {
-    document.getElementById("two").innerHTML = "HELLO"
-}
 function clicked() {
     swal({
         content: {
@@ -32,4 +30,7 @@ function clicked() {
         },
       });
 }
+
+// let points = 0
+// let score = document.getElementById("playerOne")
 
