07bf93abc621264ae1069b5ecae6ea48ca951be5
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-8.local>
2018-10-15 12:55:38 -0400

    commit read Me(Version 2)

--- ReadMe.md
+++ ReadMe.md
@@ -1,10 +1,10 @@
- #H1 Project Description
- #H3 Build a simple game as a Web Application. To demonstrate your ability to use HTML and CSS to build and style an intuitive web application and to demonstrate your competancy in DOM manipulation using JavaScript.
+ # Project Description
+ ### Build a simple game as a Web Application. To demonstrate your ability to use HTML and CSS to build and style an intuitive web application and to demonstrate your competancy in DOM manipulation using JavaScript.
 
 
-#H1 Jeopardy 
-#H4 POP is Jeopardy with a twist! POP uses present day events and topics and allows the user to test what they know. Need a break from work or school? Play POP for fun brain teasers!
-#H2 How To Play
+# Jeopardy 
+## POP is Jeopardy with a twist! POP uses present day events and topics and allows the user to test what they know. Need a break from work or school? Play POP for fun brain teasers!
+## How To Play
 1. Play with a group or alone. 
 2. Pick a category and a point value (100-300)
 4. You must input your answer to the question into the input field. But remember to always put your answer in the form of a question. (Who, What, etc..)
@@ -12,14 +12,29 @@
 6. The card/point value will turn into  a solid color block once the question has been answered, you may not answer a question twice.
 7. Countinue to play until all questions have been answered and the cards turn into solid color blocks
 
-#H2 Wireframe
-#H3 Check Out My Wireframe Here:
-[I'm an inline-style link](https://www.figma.com/file/axUzp6xUjL4zRmeEvrA4oO69/Untitled?node-id=0%3A1)
+## Technologies (Libraries and Frameworks)
+1. Bootstrap
+2. Sweet Alert
+3. Google Fonts
 
-#H2 Trello Board
-#H3 Check Out My Project Trello Board Here:
-[I'm an inline-style link](https://trello.com/b/HI1xP0pf/wdi-project-one)
+## Wireframe
 
-#H2 Play 
-#H3 Check Out The Game Here: 
-[I'm an inline-style link](https://trello.com/b/HI1xP0pf/wdi-project-one)
+[Check Out My Wireframe Here:](https://www.figma.com/file/axUzp6xUjL4zRmeEvrA4oO69/Untitled?node-id=0%3A1)
+
+## Trello Board
+[Check Out My Project Trello Board Here:](https://trello.com/b/HI1xP0pf/wdi-project-one)
+
+## Play 
+[ Check Out The Game Here: ](https://trello.com/b/HI1xP0pf/wdi-project-one)
+
+
+# Version 2.0
+I would like to continue to work on this game. 
+1. Figure out a way to make the questions switch daily. 
+2. Create a second player and or implement AI
+3. Create button to reload game
+
+## References
+https://www.w3schools.com/
+https://www.pinterest.com/
+
--- app.js
+++ app.js
@@ -1,136 +1,237 @@
-// objective: create a function that will make the .card dispear and apply a backgroundcolor to the td element
-// step one: get the element apply event listener
-document.getElementById("tdOne").addEventListener("click", firstTD)
-// go to the html and grab the element (tdOne) and listen for the user to hover (mouseover) 
+// Set-Up For Functions
+// score is declared globally to be used over and over
+var score = 0
 
-// step two: create a function to apply background color and and a button to the html element
+// function to change background color of table cell
+document.getElementById("tdOne").addEventListener("click", firstTD)
+
 function firstTD() {
-    // declare function 
-    document.getElementById("tdOne").style.background = "plum"
+    // declare function to get the td element and apply these properties
+    document.getElementById("tdOne").style.background = "#EAA1CC"
     // do this : once the html element is grabbed apply css styling 
     document.getElementById("tdOne").innerHTML = "<span style= 'font-size: 40px'></span>"
-    
-    // $('#tdOne').append(`<div>
-    // <div><button onClick="clicked()">Button 2</button></div>
-    // </div>`)
-    //go get the element and add a button to it 
+
 }
+//------------function for sweetalert--------
+function clickMeOne() {
+    swal("In Stranger Things, Season 2, what name does Dustin give the strange new pet he finds in his garbage can? ", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "What is Dart") {
+                score += 100
+            }
+            document.getElementById("score1").innerHTML = score
+        });
+}
+// everything will be be the same as above for the following cards
+
 document.getElementById("tdTwo").addEventListener("click", secondTD)
-
+// #2
 function secondTD() {
-    // declare function 
     document.getElementById("tdTwo").style.background = "#EAA1CC"
-    // do this : once the html element is grabbed apply css styling 
     document.getElementById("tdTwo").innerHTML = "<span style= 'font-size: 40px'></span>"
-
 }
+
+function clickMeTwo() {
+    swal("This company has acuquried a grocery store chain", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "What is Amazon") {
+                score += 100
+            }
+            document.getElementById("score1").innerHTML = score
+        });
+}
+// #3
 document.getElementById("tdThree").addEventListener("click", thirdTD)
 
 function thirdTD() {
-    // declare function 
     document.getElementById("tdThree").style.background = "#EAA1CC"
-    // do this : once the html element is grabbed apply css styling 
     document.getElementById("tdThree").innerHTML = "<span style= 'font-size: 40px'></span>"
 }
-document.getElementById("tdFour").addEventListener("click", fourTD)
 
-function fourTD() {
-    // declare function 
+function clickMeThree() {
+    swal("The first female rapper with two Billboard Hot 100 No. 1s", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "Who is Cardi B") {
+                score += 100
+            }
+            document.getElementById("score1").innerHTML = score
+        });
+}
+// #4
+document.getElementById("tdFour").addEventListener("click", fourthTD)
+
+function fourthTD() {
     document.getElementById("tdFour").style.background = "#EAA1CC"
-    // do this : once the html element is grabbed apply css styling 
     document.getElementById("tdFour").innerHTML = "<span style= 'font-size: 40px'></span>"
 }
-document.getElementById("tdFive").addEventListener("click", fiveTD)
 
-function fiveTD() {
-    // declare function 
-    document.getElementById("tdFive").style.background = "orange"
-    // do this : once the html element is grabbed apply css styling 
+function clickMeFour() {
+    swal("It is now illegal to hold _____ while driving in Georgia", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "What is cellphone") {
+                score += 100
+            }
+            document.getElementById("score1").innerHTML = score
+        });
+}
+// #5
+document.getElementById("tdFive").addEventListener("click", fifthTD)
+
+function fifthTD() {
+    document.getElementById("tdFive").style.background = "#EAA1CC"
     document.getElementById("tdFive").innerHTML = "<span style= 'font-size: 40px'></span>"
 }
-document.getElementById("tdSix").addEventListener("click", sixTD)
 
-function sixTD() {
-    // declare function 
-    document.getElementById("tdSix").style.background = "hotpink"
-    // do this : once the html element is grabbed apply css styling 
+function clickMeFive() {
+    swal(" Which new Star Wars film came out this last year?", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "What is Last Jedi") {
+                score += 200
+            }
+            document.getElementById("score1").innerHTML = score
+        });
+}
+// #6
+document.getElementById("tdSix").addEventListener("click", sixthTD)
+
+function sixthTD() {
+    document.getElementById("tdSix").style.background = "#EAA1CC"
     document.getElementById("tdSix").innerHTML = "<span style= 'font-size: 40px'></span>"
 }
-// ------------------------------------------------------------------------------------------
-// function for question and score
-// $100
-var score = 0
-function questionOne () {
-    
-    var person = prompt("This Artist Dropped A Surprise Album With His Wife")
 
-    if( person === "Who is Jay-Z"){
-        score += 100
-    } 
-    document.getElementById("score1").innerHTML = score
+function clickMeSix() {
+    swal("  ", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "") {
+                score += 200
+            }
+            document.getElementById("score1").innerHTML = score
+        });
 }
-function questionTwo () {
-    
-    var person = prompt("The Latest Iphone To Come Out: 222 - rgh -jjjk")
+// #7
+document.getElementById("tdSeven").addEventListener("click", seventhTD)
 
-    if( person === "What is the iphone 20"){
-        score += 100
-    } 
-    document.getElementById("score1").innerHTML = score
+function seventhTD() {
+    document.getElementById("tdSeven").style.background = "orange"
+    document.getElementById("tdSeven").innerHTML = "<span style= 'font-size: 40px' 'color: whitesmoke'> DAILY DOUBLE</span>"
 }
-function questionThree () {
-    
-    var person = prompt("The Latest Iphone To Come Out: 222 - rgh -jjjk")
 
-    if( person === "What is the iphone 20"){
-        score += 100
-    } 
-    document.getElementById("score1").innerHTML = score
+function clickMeSeven() {
+    swal(" This couple dropped a surprise album this year by the name of...", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "What is the Carters") {
+                score += 200
+            }
+            document.getElementById("score1").innerHTML = score
+        });
 }
-function questionFour () {
-    
-    var person = prompt("The Latest Iphone To Come Out: 222 - rgh -jjjk")
+// #8
+document.getElementById("tdEight").addEventListener("click", eighthTD)
 
-    if( person === "What is the iphone 20"){
-        score += 100
-    } 
-    document.getElementById("score1").innerHTML = score
+function eighthTD() {
+    document.getElementById("tdEight").style.background = "#EAA1CC"
+    document.getElementById("tdEight").innerHTML = "<span style= 'font-size: 40px'></span>"
 }
-// -----------------------------------------------------------------------------------
-// $200
-function questionFive () {
-    
-    var person = prompt("The Latest Iphone To Come Out: 222 - rgh -jjjk")
 
-    if( person === "What is the iphone 20"){
-        score += 200
-    } 
-    document.getElementById("score1").innerHTML = score
+function clickMeEight() {
+    swal(" This 28-Year-Old Latina Activist Just Beat New York's Most Powerful Congressional Rep", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "Alexandria Ocasio-Cortez") {
+                score += 200
+            }
+            document.getElementById("score1").innerHTML = score
+        });
 }
-function questionSix () {
-    
-    var person = prompt("The Latest Iphone To Come Out: 222 - rgh -jjjk")
+// #9
+document.getElementById("tdNine").addEventListener("click", ninthTD)
 
-    if( person === "What is the iphone 20"){
-        score += 200
-    } 
-    document.getElementById("score1").innerHTML = score
+function ninthTD() {
+    document.getElementById("tdNine").style.background = "#EAA1CC"
+    document.getElementById("tdNine").innerHTML = "<span style= 'font-size: 40px'></span>"
 }
-function questionSeven () {
-    
-    var person = prompt("The Latest Iphone To Come Out: 222 - rgh -jjjk")
 
-    if( person === "What is the iphone 20"){
-        score += 200
-    } 
-    document.getElementById("score1").innerHTML = score
+function clickMeNine() {
+    swal(" What super hero movie grossed over 700 million dollars in 2018?", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "What is Black Panther") {
+                score += 200
+            }
+            document.getElementById("score1").innerHTML = score
+        });
 }
-function questionEight () {
-    
-    var person = prompt("The Latest Iphone To Come Out: 222 - rgh -jjjk")
 
-    if( person === "What is the iphone 20"){
-        score += 200
-    } 
-    document.getElementById("score1").innerHTML = score
+// #10
+document.getElementById("tdTen").addEventListener("click", tenthTD)
+
+function tenthTD() {
+    document.getElementById("tdTen").style.background = "#EAA1CC"
+    document.getElementById("tdTen").innerHTML = "<span style= 'font-size: 40px'></span>"
+}
+
+function clickMeTen() {
+    swal(" What super hero movie grossed over 700 million dollars in 2018?", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "What is Black Panther") {
+                score += 200
+            }
+            document.getElementById("score1").innerHTML = score
+        });
+}
+// #11
+document.getElementById("tdEleven").addEventListener("click", eleventhTD)
+
+function eleventhTD() {
+    document.getElementById("tdEleven").style.background = "#EAA1CC"
+    document.getElementById("tdEleven").innerHTML = "<span style= 'font-size: 40px'></span>"
+}
+
+function clickMeEleven() {
+    swal(" This artist had everyone in their feelings", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "Who is Drake") {
+                score += 300
+            }
+            document.getElementById("score1").innerHTML = score
+        });
+}
+// #12
+document.getElementById("tdTwelve").addEventListener("click", TwelveTD)
+
+function TwelveTD() {
+    document.getElementById("tdTwelve").style.background = "#EAA1CC"
+    document.getElementById("tdTwelve").innerHTML = "<span style= 'font-size: 40px'></span>"
+}
+
+function clickMeTwelve() {
+    swal(" ", {
+            content: "input",
+        })
+        .then((value) => {
+            if (value === "Who is Drake") {
+                score += 300
+            }
+            document.getElementById("score1").innerHTML = score
+        });
 }
